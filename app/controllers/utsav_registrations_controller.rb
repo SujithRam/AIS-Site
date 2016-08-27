11250896f344311122f2d421c00716b6cd144508
git@github.com:SujithRam/test.git <sujithram.it@gmail.com>
2016-08-27 14:18:16 +0000

    Update 2 utsav paticipants login access complete

--- app/controllers/utsav_registrations_controller.rb
+++ app/controllers/utsav_registrations_controller.rb
@@ -1,4 +1,7 @@
 class UtsavRegistrationsController < ApplicationController
+  before_action :logged_in_user, only: [:index, :destroy, :show, :edit]
+  before_action :login_admin, only:[:index, :destroy]
+  
   def new
     @utsav_registration = UtsavRegistration.new
   end
@@ -13,6 +16,49 @@ class UtsavRegistrationsController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def destroy
+    UtsavRegistration.find(params[:id]).destroy
+    flash[:success] = "Registration deleted"
+    redirect_to utsav_registrations_path
+  end
+  
+  def show
+    @utsav_registration = UtsavRegistration.find(params[:id])
+  end
+  
+  def index
+    @utsav_registrations = UtsavRegistration.paginate(page: params[:page])
+  end
+  
+  def edit
+    @utsav_registration = UtsavRegistration.find(params[:id])
+  end
+  
+  def update
+    @utsav_registration = UtsavRegistration.find(params[:id])
+    if @utsav_registration.update_attributes(utsav_registration_params)
+      flash[:success] = "Details updated"
+      redirect_to utsav_registration_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Not accessible to students. Admin Please log in"
+        redirect_to login_url
+      end
+  end
+  
+  def login_admin
+      unless User.exists?(email: session[:email].downcase)
+      redirect_to root_url
+      flash[:danger] = "Administrator is only allowed to access registrations index page." 
+      end
   end
   
   private
