3de9b7fd180264a946a58af8fbfb1f58dc286166
git@github.com:SujithRam/test.git <sujithram.it@gmail.com>
2016-09-18 15:12:18 +0000

    Regex bug fix

--- app/models/utsav_seat.rb
+++ app/models/utsav_seat.rb
@@ -4,6 +4,6 @@ class UtsavSeat < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  TAG_REG = /\A([a-z]+)(,\s*[a-z]+)*\Z/i
+  TAG_REG = /\A([a-z\s]+)(,\s*[a-z\s]+)*\Z/i
   validates :grpdetails, :allow_blank => true, format: { with: TAG_REG, :message => "Invalid format, Use comma to seperate names." }
 end
